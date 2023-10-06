b830a48dc1cbf808ec7ca1e130b956e9d94650e0
HanifNaf <70953834+HanifNaf@users.noreply.github.com>
2023-10-06 14:28:02 +0700

    navbar image to index
    Menambahkan link href untuk logo RPSL di navbar ke halaman index

--- admin/operasional.php
+++ admin/operasional.php
@@ -20,7 +20,7 @@
 ?>
 	<div class="container">	
 		<form action="" method="POST">
-			<h2 style="display: flex; float: left;">PRODUKSI BATUBARA</h2> 
+			<h2 style="display: flex; float: left;">Data Operasional</h2> 
 			<div style="display: flex; float: right" id="pencarian1">
 				<input type="text" placeholder="Cari.." name="cari" autofocus>
 				<button type="submit" class="btn-sm btn-dark" style="border:none;"><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg></button>
@@ -81,7 +81,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h4 class="modal-title">CETAK PDF DATA PRODUKSI</h4>
+            <h4 class="modal-title">CETAK PDF DATA Operasional</h4>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           </div>
           <div class="modal-body">
