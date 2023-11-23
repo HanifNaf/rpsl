b245d0b9bcead9b63d3198d06d443dcd344ffaf0
HanifNaf <70953834+HanifNaf@users.noreply.github.com>
2023-11-24 03:40:46 +0700

    Update cek_login dan database rpsl_users

--- src/cek_login.php
+++ src/cek_login.php
@@ -1,22 +1,50 @@
 <?php 
-session_start();
 require_once("../config/config.php");
-echo SITE_URL;
-echo SITE_ROOT;
-require_once(SITE_ROOT. "../src/koneksi.php");
+require_once(SITE_ROOT. "/src/koneksi.php");
 
+if(isset($_REQUEST['username'], $_REQUEST['password'])){
 	$username 	= $_REQUEST['username'];
 	$password	= $_REQUEST['password'];
 
-	$query = pg_query($koneksi_operasional, "SELECT * FROM users WHERE username='$username' AND password='$password'");
-	$cek  = pg_num_rows($query);
+	//Query database users
+	$query = "SELECT users_id, username, password, role
+			FROM users
+			WHERE username= :uname;";
+	$prep = $koneksi_users->prepare($query);
 
-	//echo $cek;
+	try{
+		$prep->execute(array(":uname"=>$username));
 
-	if($cek>0){
-		$_SESSION['username'] = $username;
-		header("location:../public/index");	
-	}else{
-		header("location:../index");
-	}			
+		$data = $prep->fetch(PDO::FETCH_ASSOC);
+
+		if($data){
+			$stored_hash = $data['password'];
+
+			if(password_verify($password, $stored_hash)){
+				session_start();
+				$_SESSION['name'] = $data['username'];
+				$_SESSION['id'] = $data['users_id'];
+				$_SESSION['role'] = $data['role'];
+				$_SESSION['logged_in'] = true;
+				header("location:../public/index.php");
+
+			} else{
+				setcookie("login_error", "Password tidak sesuai, silahkan coba kembali!", time()+3);
+				header("location: ../index.php");			
+			}
+	
+		} else{
+			setcookie("login_error", "Pengambilan data gagal, silahkan coba kembali!", time()+3);
+			header("location: ../index.php");
+		}
+
+	} catch(PDOException $e){
+		echo "PDO ERROR: ". $e->getMessage();
+	}
+
+} else{
+	setcookie("login_error", "Request Invalid, silahkan coba kembali!", time()+3);
+	header("location: ../index.php");
+}	
+
 ?>
