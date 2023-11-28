33ec8c769ecf57df37d2c4fd844cf81f77170ee2
HanifNaf <70953834+HanifNaf@users.noreply.github.com>
2023-11-28 21:58:19 +0700

    Update link dashboard

---
+++ public/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../config/config.php");
+require_once(SITE_ROOT."/src/header-admin.php");
+
+?>
+
+<body class = "container-fluid">
+<iframe title="Dashboard Operasional" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiNjIzNDkzZWEtZjlhNy00ODU5LTkzZjItM2M4OTExYzU1MDAxIiwidCI6ImExZmU1MWM3LWEyZGMtNDI3NS1iZTUzLTI3YmQzMWU4YzU1MSIsImMiOjEwfQ%3D%3D&pageName=ReportSection282ad39d071d2d2edf0b" frameborder="0" allowFullScreen="true"></iframe>
+</body>
